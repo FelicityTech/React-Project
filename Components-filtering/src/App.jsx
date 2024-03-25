c37b23fe509f5464574f73f5abb45f9cdb679037
FelicityTech <souceking@gmail.com>
2024-03-25 03:14:03 +0100

    filtering component

---
+++ Components-filtering/src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./App.css";
+import DessertsList from "./DessertsList";
+
+const desserts = [
+{
+  name: 'Cheesecake',
+  calories: 300,
+  createdAt: '2022-07-01',
+},
+{
+  name: 'Chocolate Cake',
+  calories: 400,
+  createdAt: '2022-09-01',
+},
+{
+  name: 'Ice  Cream',
+  calories: 200,
+  createdAt: '2022-01-02',
+},
+{
+  name: 'Tiramisu',
+  calories: 300,
+  createdAt: '2022-10-03',
+},
+{
+  name: 'Cheesecake bugger',
+  calories: 600,
+  createdAt: '2022-01-04',
+},
+];
+
+
+function App() {
+  return(
+    <div className="App">
+      <h2>List of low calorie desserts:</h2>
+      <DessertsList data={desserts} />
+    </div>
+  );
+}
+
+
+export default App;
